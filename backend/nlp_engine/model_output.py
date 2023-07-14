703848e15b80b942bfccb91766750d6ba54b812e
Josemon Baby <babyjosemon77@gmail.com>
2023-07-15 00:04:58 +0100

    Updated CNN with PyTorch implementation to fix predict method problems.

--- backend/nlp_engine/model_output.py
+++ backend/nlp_engine/model_output.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 import requests
 import os
+import torch
+import torch.nn as nn
 import tensorflow as tf
 import numpy as np
 import json
@@ -10,14 +12,43 @@ import tensorflow_text as text
 import mysql.connector
 import pandas as pd
 from flask import Flask
+import pickle
 from flask import request, jsonify, Response
+
+
 
 app = Flask(__name__)
 
 load_dotenv()
 
+class CNNModel(nn.Module):
+    def __init__(self, embedding_matrix, num_classes):
+        super(CNNModel, self).__init__()
+        self.embedding = nn.Embedding.from_pretrained(torch.FloatTensor(embedding_matrix), freeze=True)
+        self.conv1 = nn.Conv1d(100, 32, kernel_size=3)
+        self.pool1 = nn.MaxPool1d(3)
+        self.dropout1 = nn.Dropout(0.3)
+        self.conv2 = nn.Conv1d(32, 64, kernel_size=3)
+        self.pool2 = nn.MaxPool1d(5)
+        self.dropout2 = nn.Dropout(0.3)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(384, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+        
+    def forward(self, x):
+        embedded = self.embedding(x)
+        embedded = embedded.permute(0, 2, 1)
+        conv1_output = self.pool1(torch.relu(self.conv1(embedded)))
+        conv1_output = self.dropout1(conv1_output)
+        conv2_output = self.pool2(torch.relu(self.conv2(conv1_output)))
+        conv2_output = self.dropout2(conv2_output)
+        flattened = self.flatten(conv2_output)
+        fc1_output = torch.relu(self.fc1(flattened))
+        logits = self.fc2(fc1_output)
+        return logits
+
 # mise en place
-savedModels={1: "savedModels/CNN_Model", 2: "savedModels/LSTM_Model", 'goemotion': "savedModels/TransformerSentiment"}
+savedModels={1: "savedModels/CNN_Model_Torch.pth", 2: "savedModels/LSTM_Model", 'goemotion': "savedModels/TransformerSentiment"}
 MAX_EMOTIONS_LENGTH=4
 
 @app.route("/api/callmodel", methods=['POST'])
@@ -143,10 +174,22 @@ def sendEmotionResultSQL(json_data, jobID):
 
 def predictions(padded_sequences, testCorpus, model_id, jobID):
     class_labels = ['Hateful', 'Non-Hateful', 'Neutral']
-    loaded_model = tf.keras.models.load_model(savedModels[int(model_id)])   
+    torch.manual_seed(42)
+    np.random.seed(42)
+    with open('embedding_matrix.pickle', 'rb') as handle:
+        embedding_matrix = pickle.load(handle)
+    input_tensor = torch.tensor(padded_sequences, dtype=torch.long)
+    loaded_model = CNNModel(embedding_matrix, num_classes=3)
+    loaded_model.load_state_dict(torch.load(savedModels[int(model_id)]))
+    loaded_model.eval()
+    with torch.no_grad():
+        val_outputs = loaded_model(input_tensor)
+        val_predictions = torch.argmax(val_outputs, dim=1).tolist()
+    predicted_classes = np.array(val_predictions)
+    #loaded_model = tf.keras.models.load_model(savedModels[int(model_id)])   
     emotion_model = tf.keras.models.load_model(savedModels['goemotion'])
-    predictions = loaded_model.predict(padded_sequences)
-    predicted_classes = np.argmax(predictions, axis=1)
+    #predictions = loaded_model.predict(padded_sequences)
+    #predicted_classes = np.argmax(predictions, axis=1)
     prediction_summary = {label: 0 for label in class_labels}
     for predicted_class in predicted_classes:
         predicted_label = class_labels[predicted_class]
