0d9e4e234ad357f30c81d5cd6a1f54d79118c677
VincentWang1024 <91959354+VincentWang1024@users.noreply.github.com>
2023-07-31 01:04:43 +0800

    producer update (#93)
    * producer update
    
    * Update aws.yml
    
    * Update aws.yml
    
    * Update aws.yml

--- backend/broker_service/consumer/main.py
+++ backend/broker_service/consumer/main.py
@@ -6,11 +6,17 @@ import os
 load_dotenv()
 
 # Load environment variables
-bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS")
-security_protocol = os.getenv("SECURITY_PROTOCOL")
-sasl_mechanisms = os.getenv("SASL_MECHANISMS")
-sasl_username = os.getenv("SASL_USERNAME")
-sasl_password = os.getenv("SASL_PASSWORD")
+# bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS")
+# security_protocol = os.getenv("SECURITY_PROTOCOL")
+# sasl_mechanisms = os.getenv("SASL_MECHANISMS")
+# sasl_username = os.getenv("SASL_USERNAME")
+# sasl_password = os.getenv("SASL_PASSWORD")
+
+bootstrap_servers = 'pkc-z9doz.eu-west-1.aws.confluent.cloud:9092'
+security_protocol = 'SASL_SSL'
+sasl_mechanisms = 'PLAIN'
+sasl_username = 'PR6L4XJHNR3TI33I'
+sasl_password = 'xs38vTm+VwsTO9/VZwmHh4DQgyORiVx5K7LlkGDqLy02llk4mrWpvqdRF5+BZOJc'
 
 # Create a Kafka producer configuration
 conf = {
